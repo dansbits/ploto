0a08f86a231231ffe664a50a354190eddec6e8c0
Dan Carpenter <daniel.carpenter01@gmail.com>
2020-06-01 23:43:57 -0400

    Add x and y axis labels

--- lib/ploto/horizontal_axis.rb
+++ lib/ploto/horizontal_axis.rb
@@ -1,14 +1,25 @@
 module Ploto
   class HorizontalAxis
 
-    attr_accessor :pixel_width
+    CHARACTER_HEIGHT = 18
+    CHARACTER_WIDTH = 9.9055
+    TICK_HEIGHT = 3
+    TICK_PADDING = 3
+    LABEL_TOP_PADDING = 10
+    
+    attr_reader :pixel_width
 
-    def initialize(min_value, max_value, pixel_width)
+    def initialize(min_value, max_value, label: nil, pixel_width: nil)
       @min_value = min_value
       @max_value = max_value
-      @pixel_width = pixel_width
-      @plottable_pixels = pixel_width * 0.94
-      @padding = pixel_width * 0.03
+      self.pixel_width = pixel_width if pixel_width
+      @label = label
+    end
+
+    def pixel_width=(pixels)
+      @pixel_width = pixels
+      @plottable_pixels = pixels * 0.94
+      @padding = pixels * 0.03
     end
 
     def pixel_position(value)
@@ -24,13 +35,21 @@ module Ploto
       end
     end
 
+    def pixel_height
+      height = CHARACTER_HEIGHT / 2.0 + TICK_HEIGHT + TICK_PADDING
+      if @label
+        height += CHARACTER_HEIGHT / 2.0 + LABEL_TOP_PADDING
+      end
+      height
+    end
+
     def render(x,y)
       container = REXML::Element.new 'svg'
       container.add_attributes({
         'x' => x,
         'y' => y,
         'width' => @pixel_width,
-        'height' => 20,
+        'height' => pixel_height,
         'overflow' => 'visible',
         'class' => 'horizontal-axis'
       })
@@ -41,20 +60,20 @@ module Ploto
           "stroke-width" => "1",
           "x1" => 0,
           "y1" => 0,
-          "x2" => @pixel_width,
+          "x2" => pixel_width,
           "y2" => 0
         }
       )
 
-      labels.each do |label|
+      labels.each do |tick_label|
         # Vertical lines for x-ticks are rendered to the right of the x position. Using one pixel to the left centers
         # the x tick in line with the points on the chart
-        x_position = label[:pixel_position] - 1
+        x_position = tick_label[:pixel_position] - 1
 
-        label_width = label[:label].to_s.length * 9.9055 # approximate pixels per character
+        label_width = tick_label[:label].to_s.length * CHARACTER_WIDTH
         text_position = x_position - (label_width / 2)
-        el = container.add_element('text', 'x' => text_position, 'y' => 15)
-        el.text = label[:label]
+        el = container.add_element('text', 'x' => text_position, 'y' => TICK_HEIGHT + TICK_PADDING + CHARACTER_HEIGHT / 2.0)
+        el.text = tick_label[:label]
 
         container.add_element(
           'line', 
@@ -63,9 +82,15 @@ module Ploto
             'x1' => x_position,
             'y1' => 0,
             'x2' => x_position,
-            'y2' => 3
+            'y2' => TICK_HEIGHT
           }
         )
+      end
+
+      if @label
+        el = container.add_element('text', 'x' => '50%', 'y' => TICK_HEIGHT + TICK_PADDING + CHARACTER_HEIGHT + LABEL_TOP_PADDING, 'dx' => -(CHARACTER_WIDTH * @label.length) / 2.0)
+        el.text = @label
+        container.add_element(el)
       end
 
       container
---
+++ spec/vertical_axis_spec.rb
@@ -0,0 +1,48 @@
+require "spec_helper"
+
+module Ploto
+  describe VerticalAxis do
+    describe "#pixel_width" do
+      let(:axis) { VerticalAxis.new(0, 5, pixel_height: 100) }
+      context "with no label" do
+        it "calculates enough space for tick marks and labels" do
+          expect_characters = 3
+          expect(axis.pixel_width).to eq (VerticalAxis::CHARACTER_WIDTH * expect_characters) + VerticalAxis::TICK_WIDTH
+        end
+      end
+
+      context "with an axis label" do
+        before { axis.label = "Foo" }
+
+        it "calculates enough space for tick marks, tick labels, and axis label" do
+          expect_characters = 3
+          expected_width = (VerticalAxis::CHARACTER_WIDTH * expect_characters) + VerticalAxis::LABEL_PADDING + 
+                           VerticalAxis::TICK_WIDTH + VerticalAxis::LABEL_WIDTH
+          expect(axis.pixel_width).to eq (expected_width)
+        end
+      end
+    end
+
+    describe "#render" do
+      let(:axis) { VerticalAxis.new(0, 5, pixel_height: 100) }
+
+      it "renders an SVG with axis line, tick marks, and tick labels" do
+        xml = axis.render
+        root = xml.root
+        expect(root.name).to eq 'svg'
+        expect(root[:x]).to eq '10'
+        expect(root[:y]).to eq '10'
+        expect(root[:height]).to eq '100'
+
+        # The longest tick label should be three characters, so we expect the width of the vertical
+        # axis to be 3 characters wide, plus additional width for the axis ticks
+        expected_width = (VerticalAxis::CHARACTER_WIDTH * 3) + VerticalAxis::TICK_WIDTH
+
+        expect(root[:width]).to eq expected_width.to_s
+        expect(root.get_elements("//line[@class='axis-line']").length).to eq 1
+        expect(root.get_elements("//line[@class='tick-mark']").length).to eq 5
+        expect(root.get_elements("//text[@class='tick-label']").length).to eq 5
+      end
+    end
+  end
+end
